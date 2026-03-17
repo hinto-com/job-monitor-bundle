d767676832f2a2945d1b108270ccf18816fbbe1d
Fabiano Roberto <fabiano.roberto@hintogroup.eu>
2026-03-17 16:38:29 +0100

    chore(di): ♻️ load services.yaml in HintoJobMonitorExtension
    - Updated `.gitignore` to exclude everything in `/config/` except `services.yaml`.
    - Added `services.yaml` with service definitions for controllers and services.
    - Integrated `YamlFileLoader` in `HintoJobMonitorExtension` to load service configurations.

--- src/DependencyInjection/HintoJobMonitorExtension.php
+++ src/DependencyInjection/HintoJobMonitorExtension.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace Hinto\Bundle\JobMonitorBundle\DependencyInjection;
 
 use Hinto\Bundle\JobMonitorBundle\Service\JobMonitorService;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
 
 class HintoJobMonitorExtension extends Extension
@@ -16,6 +18,9 @@ class HintoJobMonitorExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
+        $loader->load('services.yaml');  // Carica il file services.yaml
+
         // Processa la configurazione
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
