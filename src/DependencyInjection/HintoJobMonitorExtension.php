194e60771e36a1c9e50abce576ec8264e479daae
Fabiano Roberto <fabiano.roberto@hintogroup.eu>
2026-03-17 16:10:17 +0100

    refactor(di): ♻️ simplify JobMonitorService definition in HintoJobMonitorExtension
    - Removed usage of YamlFileLoader for service definitions.
    - Replaced FQCN with import for JobMonitorService to streamline code.

--- src/DependencyInjection/HintoJobMonitorExtension.php
+++ src/DependencyInjection/HintoJobMonitorExtension.php
@@ -4,10 +4,9 @@ declare(strict_types=1);
 
 namespace Hinto\Bundle\JobMonitorBundle\DependencyInjection;
 
-use Symfony\Component\Config\FileLocator;
+use Hinto\Bundle\JobMonitorBundle\Service\JobMonitorService;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
 
 class HintoJobMonitorExtension extends Extension
@@ -17,15 +16,12 @@ class HintoJobMonitorExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
-        $loader->load('services.yaml');  // Carica il file services.yaml
-
         // Processa la configurazione
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
         // Imposta dinamicamente l'argomento per JobMonitorService
-        $serviceDefinition = $container->getDefinition(\Hinto\Bundle\JobMonitorBundle\Service\JobMonitorService::class);
+        $serviceDefinition = $container->getDefinition(JobMonitorService::class);
         $serviceDefinition->setArgument('$connection', new Reference($config['connection']));
     }
 
